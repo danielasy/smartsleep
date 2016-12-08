d090b7e2c017bf9507bde459df4810b96f098f6e
Daniela Sayuri Yassuda <danielayassuda@gmail.com>
2016-12-08 20:54:20 -0200

    all sensors post information to cloud

--- smartsleep.ino
+++ smartsleep.ino
@@ -1,102 +1,134 @@
 #include <DHT.h>
 #include <doxygen.h>
 #include <ESP8266.h>
+#include <stdlib.h>
 
-#define SSID "neon junkyard"
-#define PASSWORD "monomania"
+#define BAUDRATE    115200
+#define SSID        "Nome"
+#define PASSWORD    "Senha"
+#define DHTPIN      7               // Pin 7 is connected to DHT11
+#define DHTTYPE     DHT11           // sensor type is DHT11
+#define SOUNDPIN    A0
+#define LDRPIN      A8
+#define DELAYMS     5000
+#define NUSP        "10076585"
 
-#define DHTPIN 7          //define as DHTPIN the Pin 3 used to connect the Sensor
-#define DHTTYPE DHT11     //define the sensor used(DHT11)
-DHT dht(DHTPIN, DHTTYPE); //create an instance of DHT
+String  host  = "104.236.90.130";
+int     port  = 8000;
+String  uri   = "/restAPI/postNewInfo";
 
-String server = "104.236.90.130:8000";
-String uri = "/postNewInfo";
+ESP8266 wifi(Serial2, (uint32_t) BAUDRATE);
+DHT dht(DHTPIN, DHTTYPE);
 
-ESP8266 wifi(Serial2, (uint32_t) 115200);
+String postRequestString(String deviceID, String sensorData, String dataType){
+  String data, postRequest;
+  
+  data  = "deviceID=" + deviceID + "&location=labsoft&data=" + sensorData + "&dataType=" + dataType;
+  postRequest = "POST " + uri + " HTTP/1.1\r\n" + "Host: " + host +":"+ port + "\r\n" +
+  "User-Agent: Arduino MEGA 2560\r\n" + "Content-Type: application/x-www-form-urlencoded\r\n" +
+  "Connection: close\r\n" + "Content-Length: " + data.length() + "\r\n" + "\r\n" + data;
 
-void setup() {
-  Serial2.begin(115200);
-  Serial.begin(115200);
+  return postRequest;
+}
 
-  // Modo de operação do ESP8266
-  if(wifi.setOprToStationSoftAP()){
-    Serial.print("Modo OK\r\n");
+void post(String postRequest){
+  char buffer[256] = {0};
+  
+  if (wifi.createTCP(host, port)) {
+    Serial.println("Conexao TCP estabelecida.");
   } else {
-    Serial.print("Erro na configuracao do modo\r\n");
+    Serial.println("Erro ao criar conexao TCP!");
   }
 
-  // Conecta a rede wi-fi
+  Serial.println(postRequest);
+
+  if(wifi.send(postRequest.c_str(), postRequest.length())) {
+    Serial.println("Dado enviado a nuvem.");
+  } else {
+    Serial.println("Erro ao enviar dado a nuvem!");
+  }
+
+  if(wifi.recv(buffer, 256)) {
+    Serial.println(buffer);
+  } else {
+    Serial.println("Erro ao receber resposta do servidor!");
+  }
+}
+
+void setup() {
+  Serial2.begin(BAUDRATE);
+  Serial.begin(BAUDRATE);
+
+  // ESP8266 setup
+  if(wifi.setOprToStationSoftAP()){
+    Serial.println("Modo OK.");
+  } else {
+    Serial.println("Erro na configuracao do modo!");
+  }
+
   if(wifi.joinAP(SSID, PASSWORD)){
-    Serial.print("Conexao OK\r\n");
+    Serial.println("Conexao com wi-fi OK.");
     Serial.print("IP: ");
     Serial.println(wifi.getLocalIP().c_str());
   } else {
-    Serial.print("Erro na conexao\r\n");
+    Serial.println("Erro na conexao com wi-fi!");
+  }
+  
+  if(wifi.disableMUX()) {
+    Serial.println("Desabilita MUX.\r\n");
+  } else {
+    Serial.println("Erro ao desabilitar MUX!\r\n");
   }
 
-  // Desabilita multiplas conexoes
-  if(wifi.disableMUX()) {
-    Serial.print("Desabilita MUX\r\n");
-  } else {
-    Serial.print("Erro ao desabilitar MUX\r\n");
-  }
-//
-//  // Inicializa sensor de temperatura e umidade
-//  dht.begin();
+  // Sensor setup
+  dht.begin();
 }
 
 void loop() {
-while (Serial2.available()) {
-Serial.write(Serial2.read());
-}
-while (Serial.available()) {
-Serial2.write(Serial.read());
-}
-//  char buffer[1024] = {0};
-//  String data, postRequest;
-//
-//  char sensor[10] = {0};
-//  float h = dht.readHumidity(); 
-//  float t = dht.readTemperature(); // Celsius (default)
-//
-//  // Checar se leituras foram realizadas
-//  if (isnan(h) || isnan(t)) {    
-//    Serial.println("Failed to read from DHT sensor!");
-//    return;
-//  }
-//  Serial.print(t, 2);    //print the temperature
-//  Serial.print("\t");
-//  Serial.println(h, 2);  //print the humidity
-//  
-//  snprintf(sensor, 10, "%f", t);
-//  data  = "deviceID=DTH11&location=labsoft&data=10&dataType=temperatura";
-//  postRequest = "POST " + uri + " HTTP/1.1\r\n" + "Host: " + server + "\r\n" +
-//  "User-Agent: Arduino\r\n" + "Content-Type: application/x-www-form-urlencoded\r\n" +
-//  "Connection: close\r\n" + "Content-Length: " + data.length() + "\r\n" + "\r\n" + data;
-//
-//  // Cria conexao TCP
-//  if (wifi.createTCP("104.236.90.130", 80)) {
-//    Serial.print("Conexao TCP OK\r\n");
-//  } else {
-//    Serial.print("Erro ao criar conexao TCP\r\n");
-//  }
-//
-//  // Loga request no terminal serial
-//  Serial.println(postRequest);
-//
-//  // Envia POST
-//  if(wifi.send(postRequest.c_str(), postRequest.length())) {
-//    Serial.print("Dado enviado\r\n");
-//  } else {
-//    Serial.print("Erro ao enviar dado\r\n");
-//  }
-//
-//  // Recebe resposta do servidor
-//  if(wifi.recv(buffer, 256)) {
-//    Serial.println(buffer);
-//  } else {
-//    Serial.print("Erro ao receber dado\r\n");
-//  }
-//
-//  delay(5000);
+  String postRequest;
+
+  char sensor[10] = {0};
+  float h = dht.readHumidity(); 
+  float t = dht.readTemperature(); // Celsius (default)
+
+  float noise = ((analogRead(SOUNDPIN) - 600.0)/30.0) * 100;
+
+  int light_intensity = analogRead(LDRPIN);
+
+  // Sensor data processing
+  if (isnan(h) || isnan(t)) {    
+    Serial.println("Failed to read from DHT sensor!");
+  }
+  
+  Serial.print(t, 2);
+  Serial.print("\t");
+  Serial.print(h, 2);
+  Serial.print("\t");
+  Serial.print(noise);
+  Serial.print("\t");
+  Serial.println(light_intensity);
+  
+  // POST results to cloud
+  if (!isnan(t)) {    
+    dtostrf(t, 3, 2, sensor);
+    postRequest = postRequestString(NUSP, sensor, "temperatura");
+    post(postRequest);
+  }
+
+  if (!isnan(h)) {    
+    dtostrf(h, 3, 2, sensor);
+    postRequest = postRequestString(NUSP, sensor, "umidade");
+    post(postRequest);
+  }
+
+  dtostrf(noise, 4, 2, sensor);
+  postRequest = postRequestString(NUSP, sensor, "ruido");
+  post(postRequest);
+
+  snprintf(sensor, 10, "%d", light_intensity);
+  postRequest = postRequestString(NUSP, sensor, "luminosidade");
+  post(postRequest);
+
+  // Interval between sensor readings
+  delay(DELAYMS);
 }
